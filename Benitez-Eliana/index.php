7656b8256196e03e1a0860cdc004d97a6b07b4b6
elianabenitez <elii.benitez35@gmail.com>
2023-09-29 09:48:08 -0300

    Cree mi carpeta personal

--- Benitez-Eliana/index.php
+++ Benitez-Eliana/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hola gente como están</title>
+    <title>Hola</title>
 </head>
 <body>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, modi quia libero hic illo consequatur fuga voluptatum dolore cum amet aspernatur veritatis molestias nesciunt, optio, ullam alias odit omnis qui?</p>
