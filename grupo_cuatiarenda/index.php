8730826df9ae259a7e644f08902c72437bc48ebc
TomySa21 <tomisalinas021@gmail.com>
2023-10-02 09:58:58 -0300

    Se realizo cambios en el codigo del archivo index

--- grupo_cuatiarenda/index.php
+++ grupo_cuatiarenda/index.php
@@ -116,45 +116,7 @@ while ($fila = mysqli_fetch_assoc($resultado)) {
 } echo "</table>";
 mysqli_close($conexion);
 ?> 
-<div class="container my-2 shadow">
-			<div class="row justify-content-center align-items-center g-2">
-				<div class="col">
-					<!-- Formulario para crear un usuario -->
-					<h2>Crear Nuevo Evento</h2>
-					<form action="crud.php" method="post" class="row g-3 mb-3">
-                    <div class="col">
-						<label for="id_update" class="form-label">Nombre</label>
-						<input type="texto" name="id" id="id_update" class="form-control" required>
-					</div>
-                    <div class="col">
-						<label for="id_update" class="form-label">Fecha</label>
-						<input type="texto" name="id" id="id_update" class="form-control" required>
-					</div>
-                    <div class="col">
-						<label for="id_update" class="form-label">Hora</label>
-						<input type="texto" name="id" id="id_update" class="form-control" required>
-					</div>
-                    <div class="col">
-						<label for="id_update" class="form-label">Lugar</label>
-						<input type="texto" name="id" id="id_update" class="form-control" required>
-					</div>
-					<div class="col">
-						<label for="nuevo_nombre" class="form-label">Información</label>
-						<input type="text" name="nuevo_nombre" id="nuevo_nombre" class="form-control" required>
-					</div>
-                    <div class="col">
-						<label for="nuevo_nombre" class="form-label">Categoría</label>
-						<input type="text" name="nuevo_nombre" id="nuevo_nombre" class="form-control" required>
-					</div>
-                    <div class="col">
-						<label for="nuevo_nombre" class="form-label">Invitados</label>
-						<input type="text" name="nuevo_nombre" id="nuevo_nombre" class="form-control" required>
-					</div>
-							<input type="submit" name="create" value="Crear">
-					</form>
-				</div>
-			</div>
-		</div>
+
  
     <div id="eventos" class="container">
         <div class="card mb-4 selectable-card cursor-pointer">
