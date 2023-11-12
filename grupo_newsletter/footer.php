ff83f25b66590ba555b248039855d5e35638f403
edwardevok <edulopez4815@gmail.com>
2023-11-12 19:58:14 -0300

    creacion de login y redireccion a form de login

--- grupo_newsletter/footer.php
+++ grupo_newsletter/footer.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="styles.css">
 
 <div class="container-fluid text-center text-white mt-3 p-2 bg-custom">
-    <p>Made with love by Joan Mauri & Eduardo López <a href="admin.php" target="_blank"
+    <p>Made with love by Joan Mauri & Eduardo López <a href="login.php" target="_blank"
             rel="noopener noreferrer">Admin</a></p>
 </div>
